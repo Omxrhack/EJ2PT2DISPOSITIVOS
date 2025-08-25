f214297cef52b34f5c7cfccd0670df989d815246
Omar Bermejo Osuna <l21170203@culiacan.tecnm.mx>
2025-08-25 10:11:05 -0700

    Initial Commit

---
+++ ClaseIOS/ClaseIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaseIOSApp.swift
+//  ClaseIOS
+//
+//  Created by Omar Bermejo Osuna on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ClaseIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
